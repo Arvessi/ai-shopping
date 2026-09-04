9a132aa4f42782fed6913f638389a34ebafd9dac
Arvessi <arvis.soms@gmail.com>
2026-09-04 22:21:08 +0300

    Load CENIQ 5 comparison visual system

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import './ceniq21.css';
 import './ceniq32.css';
 import './ceniq40.css';
+import './ceniq50.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
